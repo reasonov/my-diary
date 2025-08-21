cd6721494336e5388ab093f48eaf93c246f0668a
Илья Куновский <ilua@MacBook-Air-Ila.local>
2025-08-21 20:17:48 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/my-diary/',
+  base: './', // Относительные пути для GitHub Pages
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-  },
+    assetsDir: 'assets'
+  }
 })
